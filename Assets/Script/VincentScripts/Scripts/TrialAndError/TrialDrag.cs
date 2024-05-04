1e1ff1cb357550b154f514f8c812261ae27f6209
DESKTOP-VL6SNKH\WIN10 <rveduarte@gmail.com>
2024-05-04 03:57:39 -0400

    05/04/2024 Updates: Added FUNCTIONS to the TRIALDRAG and TRIALDROP scripts.

--- Assets/Script/VincentScripts/Scripts/TrialAndError/TrialDrag.cs
+++ Assets/Script/VincentScripts/Scripts/TrialAndError/TrialDrag.cs
@@ -8,6 +8,15 @@ public class TrialDrag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     private RectTransform rectTransform2;
     private CanvasGroup canvasGroup2;
 
+    [SerializeField]
+    private GameObject Gunportal;
+
+    private void Start()
+    {
+
+        Gunportal.SetActive(false);
+    }
+
     private void Awake()
     {
         rectTransform2 = GetComponent<RectTransform>();
@@ -36,6 +45,34 @@ public class TrialDrag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+
+        
+   // }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("ENTERWORLD");
+        //if the game object we collided with is not equal to the current game object (so anything other than itself)
+        if (collision.gameObject.tag == "Player")
+        {
+            //turn dragging for this object off because we hit something
+            Gunportal.gameObject.SetActive(true);
+        }
+    }
+
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            Gunportal.gameObject.SetActive(false);
+        }
     }
 
 }
